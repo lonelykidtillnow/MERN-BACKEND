6183e85e368d82b5d59151bd6e09dbc386f0af01
RAJESH <rmurugan3174@gmail.com>
2025-05-24 14:32:38 +0530

    Delete Day-5.1

--- Day-5.1/prisma/migrations/20250521091544_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `address` on the `dummy_Student` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "dummy_Student" DROP COLUMN "address";
