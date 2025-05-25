8a62571a196b858699d646f9ef4ae089a766b5cb
RAJESH <rmurugan3174@gmail.com>
2025-05-25 19:46:59 +0530

    Prisma Relationship & Zod Request Checking

--- Day-10/index.js
+++ Day-10/index.js
@@ -12,7 +12,6 @@ app.get("/",(req,res)=>{
 })
 
 const prisma=new PrismaClient()
-app.use(express.json())
 app.get("/students",async(req,res)=>{
     //1.Data From Frontend
 
@@ -26,11 +25,10 @@ app.get("/students",async(req,res)=>{
 const {z}=require("zod")
 app.get("/students/:id",async(req,res)=>{
 
-    //creating schema to check valid id or not
+    //creating schema For Checking valid id or not
     const idschema = z.object({
-        id: z.coerce.number()
+        id: z.string()
     });
-
 
     //1.Data From Frontend
     const {id}=idschema.parse(req.params)
@@ -42,5 +40,35 @@ app.get("/students/:id",async(req,res)=>{
     })
     //3.Data TO Frontend
     res.status(200).json({message:"Students Detail List",data:details})
+})
+
+app.use(express.json())
+app.post("/studentadd",async(req,res)=>{
+
+    //Creating schema For Checking, Request is Valid or not
+    const createschema = z.object({
+        roll_no:z.number(),
+        name:z.string(),
+        age:z.number(),
+        gender:z.string(),
+        dob:z.string(),
+        bloodgroup:z.string()
+    });
+
+    //1.Data From Frontend
+    const data=createschema.parse(req.body)
+    //2.DB Logic
+    const details=await prisma.Students.create({
+        data:{
+            roll_no:parseInt(data.roll_no),
+            name:data.name,
+            age:parseInt(data.age),
+            gender:data.gender,
+            dob:data.dob,
+            bloodgroup:data.bloodgroup
+        }
+    })
+    //3.Data TO Frontend
+    res.status(200).json({message:"Created Successfully",data:details})
 })
 app.listen(3000)
